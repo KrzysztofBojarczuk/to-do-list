df5e850c59ad1cd560581a499da2c96f9280731f
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-04-21 13:35:22 +0200

    XUnit

---
+++ todoWebApi/TodoListXunit.Tests/Controller/ControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using todoWebApi.Controllers;
+using todoWebApi.Dtos;
+using todoWebApi.Models;
+using todoWebApi.Repositories;
+using Xunit;
+
+namespace TodoListXunit.Tests.Controller
+{
+    public  class ControllerTest
+    {
+        private readonly ITodoRepository _todoRepository;
+        private readonly IMapper _mapper;
+
+        public ControllerTest()
+        {
+            _todoRepository = A.Fake<ITodoRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+        [Fact]
+        public async Task TodoController_GetAllTodo_ReturnOkAsync()
+        {
+            //Arrange
+            var todos = A.Fake<ICollection<TodoGetDto>>();
+
+            var todoList = A.Fake<List<TodoGetDto>>();
+
+            A.CallTo(() => _mapper.Map<List<TodoGetDto>>(todos)).Returns(todoList);
+
+            var controller = new TodoController(_todoRepository, _mapper);
+
+            //Act
+            var result = await controller.GetAllTodo();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }        
+
+        [Fact]
+        public async Task TodoController_GetTodoById_ReturnOkAsync()
+        {
+            //Arrange
+
+            int todoId = 1;
+            var todoMap = A.Fake<Todo>();
+            var todo = A.Fake<Todo>();
+            var todoCreate = A.Fake<TodoCreateDto>();
+            var todos = A.Fake<ICollection<TodoGetDto>>();
+            var todoList = A.Fake<List<TodoGetDto>>();
+
+            A.CallTo(() => _mapper.Map<Todo>(todoCreate)).Returns(todo);
+            A.CallTo(() => _todoRepository.CreateTodoAsync(todo)).Returns(todo);
+
+
+            var controller = new TodoController(_todoRepository, _mapper);
+            ////Act
+            var resut = controller.CreateTodo(todoCreate);
+
+            //Assert
+
+            resut.Should().NotBeNull();
+
+        }
+    }
+}
---
+++ todoWebApi/TodoListXunit.Tests/Repository/TodoEfTest.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using todoWebApi.Data;
+using Microsoft.EntityFrameworkCore.InMemory;
+using todoWebApi.Models;
+using todoWebApi.Repositories;
+using Xunit;
+using FluentAssertions;
+
+namespace TodoListXunit.Tests.Repository
+{
+    public class TodoEfTest
+    {
+        private async Task<DataContext> GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Todos.CountAsync() <= 0)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    databaseContext.Todos.Add(
+                        new Todo()
+                        {
+                            Comment = "Nauka Testów"
+
+                        });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+
+
+        }
+        [Fact]
+        public async Task GetTodoById()
+        {
+            //Arrange
+           var todo = new Todo()
+           {
+               Comment = "Nauka Testów"
+           };
+
+            //Act 
+
+            var dbContext = await GetDatabaseContext();
+            var todoRepository = new TodoRepository(dbContext);
+
+            //Assert
+
+            var result = todoRepository.CreateTodoAsync(todo);
+
+            //Assert
+            result.Should();
+
+        }
+        [Fact]
+        public async void TodoRepository_GetPokemonRating_ReturnDecimalBetweenOneAndTen()
+        {
+            //Arrange
+            var todoId = 1;
+            var dbContext = await GetDatabaseContext();
+            var todoRepository = new TodoRepository(dbContext);
+
+
+            //Act
+            var result = todoRepository.GetTodoByIdAsync(todoId);
+
+
+            //Assert
+            result.Should().NotBe(0);
+  
+        }
+    }
+}
